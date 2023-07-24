426cb08fe075c7c75131de1fe497eba326ec788e
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-07-24 23:44:52 +0200

    Added textwrap to limit the max width of ChatGPT's output, and print delay for a more chat-like appearance

--- cli-buddy.py
+++ cli-buddy.py
@@ -1,12 +1,21 @@
 import os
 import openai
+import textwrap
+import time
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+def print_response(text):
+    for line in text:
+        for char in line:
+            print(char, end='', flush=True)
+            time.sleep(0.01)
+        print()
+    print()
+
 def help():
-    print("""
-CLI-BUDDY COMMANDS
+    print("""CLI-BUDDY COMMANDS
 ------------------
     :h = Shows commands
     :q = Closes the application
@@ -22,9 +31,11 @@ commands = {":h" : help,
 print("Welcome to cli-buddy! :h for a list of useful commands.")
 while True:
     user_input = input('> ')
+    print()
     if user_input in commands.keys():
         commands[user_input]()
     else:
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": user_input}])
         ai_response = chat_completion['choices'][0]['message']['content']
-        print(ai_response)
+        formatted_response = textwrap.wrap(ai_response, 150)
+        print_response(formatted_response)
