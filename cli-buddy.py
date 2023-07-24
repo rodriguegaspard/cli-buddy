fad4193aab7b7fdef2ff2ba2bc36b30497dc8d22
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-07-24 23:11:48 +0200

    Added chat functionality and help/quit commands.

--- cli-buddy.py
+++ cli-buddy.py
@@ -3,6 +3,28 @@ import openai
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
-chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Hello World!"}])
-ai_response = chat_completion['choices'][0]['message']['content']
-print("OpenAI says :\n" + ai_response)
+
+def help():
+    print("""
+CLI-BUDDY COMMANDS
+------------------
+    :h = Shows commands
+    :q = Closes the application
+          """)
+
+def quit():
+    raise SystemExit
+
+commands = {":h" : help,
+            ":q" : quit
+            }
+
+print("Welcome to cli-buddy! :h for a list of useful commands.")
+while True:
+    user_input = input('> ')
+    if user_input in commands.keys():
+        commands[user_input]()
+    else:
+        chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": user_input}])
+        ai_response = chat_completion['choices'][0]['message']['content']
+        print(ai_response)
