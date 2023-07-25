16925878328532247b4d2569405ee4c827e66bb5
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-07-25 19:59:27 +0200

    Added weather command, which can give the current weather for any location given

--- cli-buddy.py
+++ cli-buddy.py
@@ -2,22 +2,34 @@ import os
 import openai
 import textwrap
 import time
+import requests
 
-# Load your API key from an environment variable or secret management service
+# Load your API keys from environment variables
 openai.api_key = os.getenv("OPENAI_API_KEY")
+weather_api_key = os.getenv("WEATHER_API_KEY")
 
 def print_response(text):
-    for line in text:
-        for char in line:
             print(char, end='', flush=True)
             time.sleep(0.01)
         print()
-    print()
+
+# Commands
+
+def weather():
+    city = input("Enter the city name: ")
+    r = requests.get('http://api.weatherapi.com/v1/current.json?key={0}&q={1}'.format(weather_api_key, city))
+    result = r.json()
+    print("Current weather in {0}, {1}, {2} ({3}):".format(result["location"]["name"], result["location"]["region"], result["location"]["country"], result["location"]["localtime"]))
+    print("Condition: {0}. (Cloud coverage: {1}%, Visibility: {2} km)".format(result["current"]["condition"]["text"], result["current"]["cloud"], result["current"]["vis_km"]))
+    print("Temperature: {0}°C (Feels like {1}°C)".format(result["current"]["temp_c"], result["current"]["feelslike_c"]))
+    print("Windspeed: {0} km/h, {1}° {2}".format(result["current"]["wind_kph"], result["current"]["wind_degree"], result["current"]["wind_dir"]))
+    print("Air pressure: {0} mb, Humidity: {1}%, Precipitation: {2} mm".format(result["current"]["pressure_mb"], result["current"]["humidity"], result["current"]["precip_mm"]))
 
 def help():
     print("""CLI-BUDDY COMMANDS
 ------------------
     :h = Shows commands
+    :w = Get weather info
     :q = Closes the application
           """)
 
@@ -25,17 +37,16 @@ def quit():
     raise SystemExit
 
 commands = {":h" : help,
-            ":q" : quit
+            ":w" : weather,
+            ":q" : quit,
             }
 
 print("Welcome to cli-buddy! :h for a list of useful commands.")
 while True:
-    user_input = input('> ')
     print()
     if user_input in commands.keys():
         commands[user_input]()
     else:
         chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": user_input}])
         ai_response = chat_completion['choices'][0]['message']['content']
-        formatted_response = textwrap.wrap(ai_response, 100)
         print_response(formatted_response)
